ddeb3cd5769b1b385695cc2c3c0ba1b33ccd253b
Reinaldo <reinaldoybk@hotmail.com>
2023-09-27 11:50:26 -0300

    Add StudentsController methods logic with Service

--- src/main/java/com/rei/controllers/StudentsController.java
+++ src/main/java/com/rei/controllers/StudentsController.java
@@ -1,38 +1,51 @@
 package com.rei.controllers;
 
-import com.rei.models.dto.StudentsDto;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.rei.models.dto.StudentDto;
+import com.rei.services.StudentsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RequestMapping("/api/students/v1")
 public class StudentsController {
 
-    public List<StudentsDto> findAll() {
+    @Autowired
+    private StudentsService service;
 
-        return new ArrayList<>();
+    @GetMapping
+    public List<StudentDto> findAll() {
 
-    }
-
-    public StudentsDto findById(Long id) {
-
-        return new StudentsDto();
+        return service.findAll();
 
     }
 
-    public StudentsDto create(StudentsDto studentsDto) {
+    @GetMapping("/{id}")
+    public StudentDto findById(@PathVariable("id") Long id) {
 
-        return studentsDto;
-
-    }
-
-    public StudentsDto update(StudentsDto studentsDto) {
-
-        return studentsDto;
+        return service.findById(id);
 
     }
 
-    public void delete(Long id) {}
+    @PostMapping
+    public StudentDto create(@RequestBody StudentDto studentDto) {
+
+        return service.create(studentDto);
+
+    }
+
+    @PutMapping
+    public StudentDto update(@RequestBody StudentDto studentDto) {
+
+        return service.update(studentDto);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(Long id) {
+
+        service.delete(id);
+
+    }
 
 }
