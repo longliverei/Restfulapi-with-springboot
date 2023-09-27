6ae964aee8f39670e6af28c2b6200363b78a0dbf
Reinaldo <reinaldoybk@hotmail.com>
2023-09-27 09:47:36 -0300

    Add methods(without service) to StudentsController

--- src/main/java/com/rei/controllers/StudentsController.java
+++ src/main/java/com/rei/controllers/StudentsController.java
@@ -1,2 +1,38 @@
-package com.rei.controllers;public class StudentsController {
+package com.rei.controllers;
+
+import com.rei.models.dto.StudentsDto;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequestMapping("/api/students/v1")
+public class StudentsController {
+
+    public List<StudentsDto> findAll() {
+
+        return new ArrayList<>();
+
+    }
+
+    public StudentsDto findById(Long id) {
+
+        return new StudentsDto();
+
+    }
+
+    public StudentsDto create(StudentsDto studentsDto) {
+
+        return studentsDto;
+
+    }
+
+    public StudentsDto update(StudentsDto studentsDto) {
+
+        return studentsDto;
+
+    }
+
+    public void delete(Long id) {}
+
 }
