81a03df406cb2f7fd5deea1257823a2c3a276b3a
Reinaldo <reinaldoybk@hotmail.com>
2023-09-27 11:25:54 -0300

    Refactor repository

--- src/main/java/com/rei/repository/StudentsRepository.java
+++ src/main/java/com/rei/repository/StudentsRepository.java
@@ -1,6 +1,6 @@
 package com.rei.repository;
 
-import com.rei.models.entity.Students;
+import com.rei.models.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudentsRepository extends JpaRepository<Students, Long> {}
+public interface StudentsRepository extends JpaRepository<Student, Long> {}
