4e88998200d8ac14020255ac4371600398633ffe
Reinaldo <reinaldoybk@hotmail.com>
2023-09-27 09:40:20 -0300

    Create StudentsDto

---
+++ src/main/java/com/rei/models/dto/StudentsDto.java
@@ -0,0 +1,65 @@
+package com.rei.models.dto;
+
+import java.util.Objects;
+
+public class StudentsDto {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String course;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StudentsDto that = (StudentsDto) o;
+        return Objects.equals(id, that.id) && Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName) && Objects.equals(email, that.email) && Objects.equals(course, that.course);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, email, course);
+    }
+}
