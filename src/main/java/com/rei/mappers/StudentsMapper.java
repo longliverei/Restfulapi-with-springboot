2cbed25ecd0b7dd497ca4556c81a25f7c078babc
Reinaldo <reinaldoybk@hotmail.com>
2023-09-27 11:24:51 -0300

    Create StudentsMapper and Add MapStruct dependency

---
+++ src/main/java/com/rei/mappers/StudentsMapper.java
@@ -0,0 +1,17 @@
+package com.rei.mappers;
+
+import com.rei.models.dto.StudentDto;
+import com.rei.models.entity.Student;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface StudentsMapper {
+
+    StudentDto entityToDto(Student student);
+    Student dtoToEntity(StudentDto studentDto);
+    List<StudentDto> entityListToDtoList(List<Student> students);
+    List<Student> dtoListToEntityList(List<StudentDto> dtoStudents);
+
+}
---
+++ src/main/java/com/rei/services/StudentsService.java
@@ -0,0 +1,2 @@
+package com.rei.services;public class StudentsService {
+}
