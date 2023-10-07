7ff850fa503a8f9d43668af0e539b9f7e767f52b
Reinaldo <reinaldoybk@hotmail.com>
2023-10-07 17:05:06 -0300

    Refactor entity

--- src/main/java/com/rei/models/entity/Student.java
+++ src/main/java/com/rei/models/entity/Student.java
@@ -14,7 +14,7 @@ public class Student implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "first_name", length = 100, nullable = false)
