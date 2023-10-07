35b9f93a4b2216c25627cbde32711c2ca86d56a9
Reinaldo <reinaldoybk@hotmail.com>
2023-10-07 12:45:11 -0300

    Create integration test for controller

--- src/test/java/com/rei/integrationtests/controller/StudentsControllerTest.java
+++ src/test/java/com/rei/integrationtests/controller/StudentsControllerTest.java
@@ -1,12 +1,21 @@
 package com.rei.integrationtests.controller;
 
+import com.rei.models.dto.StudentDto;
+import com.rei.services.StudentsService;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
+
+import static io.restassured.RestAssured.given;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class StudentsControllerTest {
@@ -32,4 +41,32 @@ public class StudentsControllerTest {
         registry.add("spring.datasource.username", postgres::getUsername);
         registry.add("spring.datasource.password", postgres::getPassword);
     }
+
+    @Autowired
+    StudentsService service;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI = "http://localhost:" + port;
+    }
+
+    @Test
+    void shouldCreateStudentDto() {
+        StudentDto dto = new StudentDto();
+        dto.setId(1L);
+        dto.setFirstName("Vento");
+        dto.setLastName("Áureo");
+        dto.setEmail("vento@aureo.com");
+        dto.setCourse("Ciência da Computação");
+
+        service.create(dto);
+
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/api/students/v1")
+                .then()
+                .statusCode(200);
+    }
+
 }
