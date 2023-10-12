e69079d5d9be713b439337883f85d77eaf3dbb3e
Reinaldo <reinaldoybk@hotmail.com>
2023-10-12 02:18:09 -0300

    Add disablePerson method to StudentsRepository

--- src/main/java/com/rei/repository/StudentsRepository.java
+++ src/main/java/com/rei/repository/StudentsRepository.java
@@ -2,5 +2,12 @@ package com.rei.repository;
 
 import com.rei.models.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface StudentsRepository extends JpaRepository<Student, Long> {}
+public interface StudentsRepository extends JpaRepository<Student, Long> {
+
+    @Query("UPDATE Student s set s.enabled = false WHERE s.id =:id")
+    void disablePerson(@Param("id") Long id);
+
+}
