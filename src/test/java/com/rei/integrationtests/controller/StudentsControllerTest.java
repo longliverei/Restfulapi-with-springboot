722f58e9e4a377d2f498e5f109a010d28ec2645e
Reinaldo <reinaldoybk@hotmail.com>
2023-10-12 21:09:28 -0300

    Fix tests to accept jwt token

--- src/test/java/com/rei/integrationtests/controller/StudentsControllerTest.java
+++ src/test/java/com/rei/integrationtests/controller/StudentsControllerTest.java
@@ -2,10 +2,19 @@ package com.rei.integrationtests.controller;
 
 import com.rei.integrationtests.mock.MockStudent;
 import com.rei.models.dto.StudentDto;
+import com.rei.models.dto.security.AccountCredentialsDto;
+import com.rei.models.dto.security.TokenDto;
 import com.rei.services.StudentsService;
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
 import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -31,6 +40,8 @@ public class StudentsControllerTest {
 
     @Autowired
     StudentsService service;
+
+    private static RequestSpecification specification;
 
     MockStudent mock;
 
@@ -59,52 +70,91 @@ public class StudentsControllerTest {
 
     @Test
     @Order(1)
+    public void shouldAuthenticate() {
+        AccountCredentialsDto user = new AccountCredentialsDto("admin", "admin");
+
+        var accessToken = given()
+                .basePath("/auth/signin")
+                .contentType(ContentType.JSON)
+                .body(user)
+                .when()
+                .post()
+                .then()
+                .assertThat().statusCode(200)
+                .extract().body().as(TokenDto.class).getAccessToken();
+
+        specification = new RequestSpecBuilder()
+                .addHeader("Authorization", "Bearer " + accessToken)
+                .setBasePath("/api/students/v1")
+                .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+                .build();
+    }
+
+    @Test
+    @Order(2)
     void shouldCreateStudent() {
         StudentDto dto = mock.mockStudentDto();
 
         ValidatableResponse validatableResponse = given()
+                .spec(specification)
                 .contentType(ContentType.JSON)
                 .body(dto)
                 .when()
-                .post("/api/students/v1")
+                .post()
                 .then()
-                .log().all().assertThat().statusCode(201)
+                .assertThat().statusCode(201)
                 .and()
                 .header("Location", equalTo(baseURI + "/api/students/v1/" + dto.getId()));
     }
 
     @Test
-    @Order(2)
+    @Order(3)
     void shouldGetAllStudents() {
         ValidatableResponse validatableResponse = given()
+                .spec(specification)
                 .contentType(ContentType.JSON)
                 .when()
-                .get("/api/students/v1")
+                .get()
                 .then()
                 .log().all().assertThat().statusCode(200);
     }
     @Test
-    @Order(3)
+    @Order(4)
     void shouldUpdateStudent() {
         StudentDto dto = mock.mockStudentDto();
         dto.setFirstName("Reinaldo");
 
         ValidatableResponse validatableResponse = given()
+                .spec(specification)
                 .contentType(ContentType.JSON)
                 .body(dto)
                 .when()
-                .put("/api/students/v1")
+                .put()
                 .then()
                 .log().all().assertThat().statusCode(204);
     }
 
     @Test
-    @Order(4)
-    void shouldGetAStudent() {
+    @Order(5)
+    void shouldDisableStudent() {
         ValidatableResponse validatableResponse = given()
+                .spec(specification)
                 .contentType(ContentType.JSON)
                 .when()
-                .get("/api/students/v1/1")
+                .patch("1")
+                .then()
+                .log().all().assertThat().statusCode(204);
+    }
+
+    @Test
+    @Order(6)
+    void shouldGetAStudent() {
+        ValidatableResponse validatableResponse = given()
+                .spec(specification)
+                .contentType(ContentType.JSON)
+                .when()
+                .get("1")
                 .then()
                 .log().all().assertThat().statusCode(200)
                 .and()
@@ -112,12 +162,13 @@ public class StudentsControllerTest {
     }
 
     @Test
-    @Order(5)
+    @Order(7)
     void shouldDeleteAStudent() {
         ValidatableResponse validatableResponse = given()
+                .spec(specification)
                 .contentType(ContentType.JSON)
                 .when()
-                .delete("/api/students/v1/1")
+                .delete("1")
                 .then()
                 .log().all().assertThat().statusCode(204);
     }
