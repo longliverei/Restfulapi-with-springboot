ff90233dc73e84d35d066ddab2c96404f7fd7279
Reinaldo <reinaldoybk@hotmail.com>
2023-10-12 21:08:23 -0300

    Refactor

--- src/main/java/com/rei/repository/StudentsRepository.java
+++ src/main/java/com/rei/repository/StudentsRepository.java
@@ -2,12 +2,14 @@ package com.rei.repository;
 
 import com.rei.models.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface StudentsRepository extends JpaRepository<Student, Long> {
 
-    @Query("UPDATE Student s set s.enabled = false WHERE s.id =:id")
+    @Modifying
+    @Query("UPDATE Student s SET s.enabled = false WHERE s.id =:id")
     void disablePerson(@Param("id") Long id);
 
 }
