da355cb159a7d30874e5b71e0a49323115af8a03
Reinaldo <reinaldoybk@hotmail.com>
2023-10-13 15:20:56 -0300

    Update endpoint for findByName

--- src/main/java/com/rei/controllers/StudentsController.java
+++ src/main/java/com/rei/controllers/StudentsController.java
@@ -3,7 +3,6 @@ package com.rei.controllers;
 import com.rei.models.dto.StudentDto;
 import com.rei.services.StudentsService;
 import jakarta.transaction.Transactional;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -11,8 +10,6 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/students/v1")
@@ -39,6 +36,19 @@ public class StudentsController {
     @GetMapping("/{id}")
     public StudentDto findById(@PathVariable("id") Long id) {
         return service.findById(id);
+    }
+
+    @GetMapping("/findByName/{firstName}")
+    public ResponseEntity<PagedModel<EntityModel<StudentDto>>> findStudentByName(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "12") Integer size,
+            @RequestParam(value = "direction", defaultValue = "asc") String direction,
+            @PathVariable("firstName") String firstName
+    ) {
+        var sortDirection = "desc".equalsIgnoreCase(direction) ? Sort.Direction.DESC : Sort.Direction.ASC;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, "firstName"));
+
+        return ResponseEntity.ok(service.findStudentByName(firstName, pageable));
     }
 
     @PostMapping
