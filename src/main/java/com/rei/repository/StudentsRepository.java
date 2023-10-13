a0177b193a30222d127b95c87b0d1b32c7919ac9
Reinaldo <reinaldoybk@hotmail.com>
2023-10-13 15:20:40 -0300

    Add findByName on repository

--- src/main/java/com/rei/repository/StudentsRepository.java
+++ src/main/java/com/rei/repository/StudentsRepository.java
@@ -1,15 +1,19 @@
 package com.rei.repository;
 
 import com.rei.models.entity.Student;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface StudentsRepository extends JpaRepository<Student, Long> {
-
     @Modifying
     @Query("UPDATE Student s SET s.enabled = false WHERE s.id =:id")
     void disablePerson(@Param("id") Long id);
+
+    @Query("SELECT s FROM Student s WHERE s.firstName LIKE LOWER(CONCAT('%', :firstName, '%'))")
+    Page<Student> findStudentByName(@Param("firstName") String firstName, Pageable pageable);
 
 }
