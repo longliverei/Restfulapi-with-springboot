78f5b1d6f0505bd980b24a092b8a72128ecba38c
Reinaldo <reinaldoybk@hotmail.com>
2023-10-19 10:11:59 -0300

    Add Swagger Documentation to StudentsController

--- src/main/java/com/rei/controllers/StudentsController.java
+++ src/main/java/com/rei/controllers/StudentsController.java
@@ -2,17 +2,25 @@ package com.rei.controllers;
 
 import com.rei.models.dto.StudentDto;
 import com.rei.services.StudentsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.transaction.Transactional;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.PagedModel;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/students/v1")
+@Tag(name = "Students", description = "Endpoint for managing Students")
 public class StudentsController {
 
     private final StudentsService service;
@@ -21,7 +29,21 @@ public class StudentsController {
         this.service = service;
     }
 
-    @GetMapping
+    @GetMapping(produces = { "application/json" } )
+    @Operation(summary = "Finds all students", description = "Finds all students", tags = {"Students"},
+    responses = {
+            @ApiResponse(description = "Success", responseCode = "200",
+            content = {
+                    @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = StudentDto.class))
+                    )
+            }),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+    })
     public ResponseEntity<PagedModel<EntityModel<StudentDto>>> findAll(
             @RequestParam(value = "page", defaultValue = "0") Integer page,
             @RequestParam(value = "size", defaultValue = "12") Integer size,
@@ -33,12 +55,36 @@ public class StudentsController {
         return ResponseEntity.ok(service.findAll(pageable));
     }
 
-    @GetMapping("/{id}")
+    @GetMapping(value = "/{id}", produces = { "application/json" })
+    @Operation(summary = "Find a student by Id", description = "Find a student by Id", tags = {"Students"},
+    responses = {
+            @ApiResponse(
+                    description = "Success", responseCode = "200",
+                    content = @Content(schema = @Schema(implementation = StudentDto.class))
+            ),
+            @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+    })
     public StudentDto findById(@PathVariable("id") Long id) {
         return service.findById(id);
     }
 
-    @GetMapping("/findByName/{firstName}")
+    @GetMapping(value = "/findByName/{firstName}", produces = { "application/json" })
+    @Operation(summary = "Find a student by Name", description = "Find a student by Name", tags = {"Students"},
+            responses = {
+                    @ApiResponse(
+                            description = "Success", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = StudentDto.class))
+                    ),
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            })
     public ResponseEntity<PagedModel<EntityModel<StudentDto>>> findStudentByName(
             @RequestParam(value = "page", defaultValue = "0") Integer page,
             @RequestParam(value = "size", defaultValue = "12") Integer size,
@@ -51,19 +97,58 @@ public class StudentsController {
         return ResponseEntity.ok(service.findStudentByName(firstName, pageable));
     }
 
-    @PostMapping
+    @PostMapping(produces = { "application/json" }, consumes = { "application/json" })
+    @Operation(summary = "Create a Student", description = "Create a Student", tags = {"Students"},
+            responses = {
+                    @ApiResponse(
+                            description = "Created", responseCode = "201",
+                            content = @Content(schema = @Schema(implementation = StudentDto.class))
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            })
     public ResponseEntity<?> create(@RequestBody StudentDto studentDto) { return service.create(studentDto); }
 
-    @PutMapping
+    @PutMapping(produces = { "application/json" }, consumes = { "application/json" })
+    @Operation(summary = "Update a Student", description = "Update a Student", tags = {"Students"},
+            responses = {
+                    @ApiResponse(
+                            description = "Updated", responseCode = "204",
+                            content = @Content(schema = @Schema(implementation = StudentDto.class))
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            })
     public ResponseEntity<?> update(@RequestBody StudentDto studentDto) { return service.update(studentDto); }
 
     @Transactional
-    @PatchMapping("/{id}")
+    @PatchMapping(value = "/{id}", produces = { "application/json" }, consumes = { "application/json" })
+    @Operation(summary = "Disable a Student", description = "Disable a Student", tags = {"Students"},
+            responses = {
+                    @ApiResponse(
+                            description = "Disabled", responseCode = "204",
+                            content = @Content(schema = @Schema(implementation = StudentDto.class))
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            })
     public ResponseEntity<?> disablePerson(@PathVariable("id") Long id) {
         return service.disablePerson(id);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a Student", description = "Delete a Student", tags = {"Students"},
+            responses = {
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            })
     public ResponseEntity<?> delete(@PathVariable("id") Long id) {
         return service.delete(id);
     }
